7ec2e2925b2249b6a22eb9ca206f61c0859fdedf
Alex Chirino <alexchirino3@gmail.com>
2021-04-19 03:31:16 -0500

    [ADD] Header component

---
+++ src/components/Web/Header/PageHeader.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout, Menu } from 'antd';
+
+const { Header } = Layout
+
+const PageHeader = () => {
+  return (
+    <Header>
+      <div className="logo">
+        {/* Here goes the logo */}
+      </div>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+  )
+}
+
+export default PageHeader
