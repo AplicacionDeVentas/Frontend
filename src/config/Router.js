311ba889f7c4c527f522209344a4a66824b6880a
Alex Chirino <alexchirino3@gmail.com>
2021-04-26 22:02:51 -0500

    [UPDATE] Add /login route

--- src/config/Router.js
+++ src/config/Router.js
@@ -3,6 +3,7 @@ import LayoutHome from "../layout/LayoutHome";
 
 // pages
 import Home from "../pages/Home";
+import Login from "../components/Web/User/Login/Login"
 
 //components
 import Error404 from "../components/Error/Error404";
@@ -16,6 +17,11 @@ const router = [
             {
                 path: "/",
                 component: Home,
+                exact: true
+            },
+            {
+                path: "/login",
+                component: Login,
                 exact: true
             },
             {
