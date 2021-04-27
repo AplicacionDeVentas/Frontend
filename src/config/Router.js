d167eb1cf06a68bdba4cbd5e89d82bbd3544670f
Alex Chirino <alexchirino3@gmail.com>
2021-04-27 02:14:03 -0500

    [UPDATE] Add the /signup route

--- src/config/Router.js
+++ src/config/Router.js
@@ -4,6 +4,7 @@ import LayoutHome from "../layout/LayoutHome";
 // pages
 import Home from "../pages/Home";
 import Login from "../components/Web/User/Login/Login"
+import SignUp from "../components/Web/User/SignUp/SignUp"
 
 //components
 import Error404 from "../components/Error/Error404";
@@ -22,6 +23,11 @@ const router = [
             {
                 path: "/login",
                 component: Login,
+                exact: true
+            },
+            {
+                path: "/signup",
+                component: SignUp,
                 exact: true
             },
             {
