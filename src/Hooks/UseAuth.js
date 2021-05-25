ddfa22fd6e2f48489e1e9b3c536d0707406f973d
Camilo Vargas <camilovargasingm@gmail.com>
2021-05-24 21:49:26 -0500

    add traking files

---
+++ src/Hooks/UseAuth.js
@@ -0,0 +1,6 @@
+import {useContext} from "react";
+import {AuthContext} from "../Providers/AuthProviders";
+
+const Export = () => useContext(AuthContext);
+
+export default Export;
---
+++ src/Providers/AuthProviders.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect, createContext} from "react";
+import {auth, db} from "../config/FirebaseConfig"
+
+
+export const AuthContext = createContext();
+
+export default function AuthProvider(props) {
+    const {children} = props;
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            if(user){
+                getDataUser(user.uid)
+            }
+        })
+    }, [])
+
+
+    const getDataUser = async (uid) => {
+        const userDataPromesa = await db.collection('user').doc(uid).get();
+        setUserData(userDataPromesa.data());
+    }
+
+    return <AuthContext.Provider value={userData} >{children}</AuthContext.Provider>
+}
+
